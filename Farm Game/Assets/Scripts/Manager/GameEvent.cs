84d7d1485c1d03bfcb8426759b9c8c4de07031e0
chirilcristina <chirilcristina2002@gmail.com>
2024-08-27 15:16:15 +0300

    Inca niste mici schimbari prin scripturi

--- Farm Game/Assets/Scripts/Manager/GameEvent.cs
+++ Farm Game/Assets/Scripts/Manager/GameEvent.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public abstract class GameEvent : MonoBehaviour
+public class GameEvent
 {
 
 }
--- Farm Game/Assets/Scripts/Shop/ShopManager.cs
+++ Farm Game/Assets/Scripts/Shop/ShopManager.cs
@@ -80,7 +80,6 @@ public class ShopManager : MonoBehaviour
             {
                 ShopItem item = shopItems[key][j];
 
-                
                 if (item.Level == info.newLvl)
                 {
                     shopTabs.transform.GetChild(i).GetChild(j).GetComponent<ShopItemHolder>().UnlockItem();
