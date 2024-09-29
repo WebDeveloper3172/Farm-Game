585c439a4f885edb33106f434b21fbc08097a2db
chirilcristina <chirilcristina2002@gmail.com>
2024-09-29 14:57:41 +0300

    Edit PanZoom Script

--- Farm Game/Assets/Scripts/Manager/CurencySystem.cs
+++ Farm Game/Assets/Scripts/Manager/CurencySystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -21,13 +22,40 @@ public class CurencySystem : MonoBehaviour
     }
     private void Start()
     {
+        CurrencyAmounts[CurrencyType.Coins] = 100;
+        CurrencyAmounts[CurrencyType.Crystals] = 10;
+        UpdateUI();
+
         EventManager.Instance.AddListener<CurrencyChangeGameEvent>(OnCurrencyChange);
         EventManager.Instance.AddListener<NotEnoughCurrencyGameEvent>(OnNotEnough);
     }
+
+    private void UpdateUI()
+    { 
+        for(int i = 0; i < texts.Count; i++)
+        {
+            currencyText[(CurrencyType)i].text = CurrencyAmounts[(CurrencyType)i].ToString();
+
+        }
+    }
+
     private void OnCurrencyChange(CurrencyChangeGameEvent info)
     {
+        if (info.amount < 0)
+        {
+            if (CurrencyAmounts[info.currencyType] < Math.Abs(info.amount))
+            {
+                EventManager.Instance.QueueEvent(new NotEnoughCurrencyGameEvent(info.amount , info.currencyType));
+                return;
+            }
+
+            EventManager.Instance.QueueEvent(new EnoughCurrencyGameEvent());
+        }
+
         CurrencyAmounts[info.currencyType] += info.amount;
         currencyText[info.currencyType].text = CurrencyAmounts[info.currencyType].ToString();
+
+        UpdateUI();
     }
 
     private void OnNotEnough(NotEnoughCurrencyGameEvent info)
